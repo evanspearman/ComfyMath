d42a49d233dd8a68e57ed1dd35ae87623db77e1d
Evan Spearman <evan@spearman.mb.ca>
2023-07-29 08:09:26 -0500

    feat: Added boolean logic nodes

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from .src.comfymath.convert import NODE_CLASS_MAPPINGS as convert_NCM
+from .src.comfymath.bool import NODE_CLASS_MAPPINGS as bool_NCM
 from .src.comfymath.int import NODE_CLASS_MAPPINGS as int_NCM
 from .src.comfymath.float import NODE_CLASS_MAPPINGS as float_NCM
 from .src.comfymath.number import NODE_CLASS_MAPPINGS as number_NCM
@@ -8,6 +9,7 @@ from .src.comfymath.control import NODE_CLASS_MAPPINGS as control_NCM
 
 NODE_CLASS_MAPPINGS = {
     **convert_NCM,
+    **bool_NCM,
     **int_NCM,
     **float_NCM,
     **number_NCM,
---
+++ src/comfymath/bool.py
@@ -0,0 +1,59 @@
+from typing import Any, Callable, Mapping
+
+DEFAULT_BOOL = ("BOOL", {"default": False})
+
+
+BOOL_UNARY_OPERATIONS: Mapping[str, Callable[[bool], bool]] = {
+    "Not": lambda a: not a,
+}
+
+BOOL_BINARY_OPERATIONS: Mapping[str, Callable[[bool, bool], bool]] = {
+    "Nor": lambda a, b: not (a or b),
+    "Xor": lambda a, b: a ^ b,
+    "Nand": lambda a, b: not (a and b),
+    "And": lambda a, b: a and b,
+    "Xnor": lambda a, b: not (a ^ b),
+    "Or": lambda a, b: a or b,
+    "Eq": lambda a, b: a == b,
+    "Neq": lambda a, b: a != b,
+}
+
+
+class BoolUnaryOperation:
+    @classmethod
+    def INPUT_TYPES(cls) -> Mapping[str, Any]:
+        return {
+            "required": {"op": (list(BOOL_UNARY_OPERATIONS.keys()),), "a": DEFAULT_BOOL}
+        }
+
+    RETURN_TYPES = ("BOOL",)
+    FUNCTION = "op"
+    CATEGORY = "math/bool"
+
+    def op(self, op: str, a: bool) -> tuple[bool]:
+        return (BOOL_UNARY_OPERATIONS[op](a),)
+
+
+class BoolBinaryOperation:
+    @classmethod
+    def INPUT_TYPES(cls) -> Mapping[str, Any]:
+        return {
+            "required": {
+                "op": (list(BOOL_BINARY_OPERATIONS.keys()),),
+                "a": DEFAULT_BOOL,
+                "b": DEFAULT_BOOL,
+            }
+        }
+
+    RETURN_TYPES = ("BOOL",)
+    FUNCTION = "op"
+    CATEGORY = "math/bool"
+
+    def op(self, op: str, a: bool, b: bool) -> tuple[bool]:
+        return (BOOL_BINARY_OPERATIONS[op](a, b),)
+
+
+NODE_CLASS_MAPPINGS = {
+    "CM_BoolUnaryOperation": BoolUnaryOperation,
+    "CM_BoolBinaryOperation": BoolBinaryOperation,
+}
